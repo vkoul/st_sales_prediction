9897e529f28b37edcb889b7676c37a7c9ec35708
Vikesh Koul <vikeshkoul@gmail.com>
2022-09-21 00:56:43 +0200

    Add files via upload

---
+++ st_app.py
@@ -0,0 +1,41 @@
+# Imports
+import streamlit as st
+import pandas as pd
+import pickle
+
+# Opening intro text
+st.write("# Predict Sales✨")
+
+st.write("### Determine the scenario 🎛️:")
+
+# Price of the product
+price = st.slider('Price of the product?💲', min_value=3, max_value=15, value=7, step=1)
+
+# Advertisment budget
+ads = st.slider('What is the Adv budget?💰', min_value=35, max_value=65, value=50, step = 2)
+
+
+# Promotions
+promo = st.slider('What is the promotional budget?💰', min_value=35, max_value=65, value=45, step = 2)
+
+
+# Creating the dataframe to run predictions on
+row = [price, ads, promo]
+columns = ['dollar_price', 'advertisment', 'promotions']
+
+mktg_scenario = pd.DataFrame(dict(zip(columns, row)), index=[0])
+
+# Show the table?
+# st.table(mktg_scenario)
+
+# Now predicting!
+if st.button(label="Click to Predict"):
+
+    # Load the model
+    loaded_model = pickle.load(open('lm_model_prediction.sav','rb'))
+    
+    # Make predictions (and get out pred probabilities)
+    pred = loaded_model.predict(mktg_scenario)[0]
+    
+    st.write(f"Predicted Unit Sales📊: {pred:,.0f} units ")
+
