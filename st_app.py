68e82e7c9f61766da28d7d48a45bb8cb74e5eee6
Vikesh Koul <vikeshkoul@gmail.com>
2023-05-15 01:22:26 +0200

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -11,21 +11,21 @@ st.set_page_config(page_title = "Sales Prediction"
 # Opening intro text
 st.write("# Predict Sales✨")
 
-# with st.expander("About this app"):
+with st.expander("About this app"):
 
-#     st.write("")
+    st.write("")
 
-#     st.markdown(
-#         """
+    st.markdown(
+        """
 
-#     This is a simulated dataset for a Case Study to demonstrate the usage of Linear Regression to solve business problem. 
+    This is a simulated dataset for a Case Study to demonstrate the usage of Linear Regression to solve business problem. 
     
-#     The data spans 24 months and has `unit sales` along with the `price`, `advertisment spend` and `promotion spend`
+    The data spans 24 months and has `unit sales` along with the `price`, `advertisment spend` and `promotion spend`
     
-#     The predictions are based on a Linear Regression model . It is used to establish the relationship between the independent variables (price, promotion and Advertisment) to predict the unit sales that will be sold for each given scenario.
+    The predictions are based on a Linear Regression model . It is used to establish the relationship between the independent variables (price, promotion and Advertisment) to predict the unit sales that will be sold for each given scenario.
     
-#     """
-#     )
+    """
+    )
 
 st.write("### Determine the scenario 🎛️:")
 
