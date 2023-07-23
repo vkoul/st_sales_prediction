2f436e6e32367359334b9906e18f1af7b558ac45
Vikesh Koul <vikeshkoul@gmail.com>
2023-07-23 12:03:26 +0200

    updated emojis

--- st_app.py
+++ st_app.py
@@ -33,11 +33,11 @@ st.write("### Determine the scenario 🎛️:")
 price = st.slider('Price of the product?💲', min_value=3, max_value=15, value=7, step=1)
 
 # Advertisment budget
-ads = st.slider('What is the Adv budget?💰', min_value=35, max_value=65, value=50, step = 1)
+ads = st.slider('What is the Adv budget?📢', min_value=35, max_value=65, value=50, step = 1)
 
 
 # Promotions
-promo = st.slider('What is the promotional budget?💰', min_value=35, max_value=65, value=45, step = 1)
+promo = st.slider('What is the promotional budget?💥', min_value=35, max_value=65, value=45, step = 1)
 
 
 # Creating the dataframe to run predictions on
@@ -58,5 +58,5 @@ if st.button(label="Click to predict unit sales"):
     # Make predictions (and get out pred probabilities)
     pred = loaded_model.predict(mktg_scenario)[0]
     
-    st.write(f"Predicted Unit Sales📊: {pred:,.0f} units ")
+    st.write(f"Predicted Unit Sales💰: {pred:,.0f} units ")
 
